6243b04ced2e55560326ec15f8bcf02759742c85
Boyan <torosov@gmail.com>
2016-04-09 22:50:27 +0300

    Login and Register forms added

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('issueTracker', [
+    'ngRoute',
+    'issueTracker.home'
+])
+    .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/'});
+}])
+    .constant('BASE_URL','http://softuni-social-network.azurewebsites.net/api')
---
+++ app/users/authentication.js
@@ -0,0 +1,39 @@
+angular.module('issueTracker.users.authentication', [])
+    .factory('authentication', ['$http', '$q', 'BASE_URL', function ($http, $q, BASE_URL) {
+
+        function registerUser(user) {
+            var deferred = $q.defer();
+
+            $http.post(BASE_URL + '/users/Register', user)
+                .then(function (response) {
+                    deferred.resolve(response.data)
+                }, function (error) {
+
+                })
+
+            return deferred.promise
+        }
+
+        function loginUser(user) {
+            var deferred = $q.defer();
+
+            $http.post(BASE_URL + '/users/Login', user)
+                .then(function (response) {
+                    deferred.resolve(response.data)
+                }, function (error) {
+
+                })
+
+            return deferred.promise
+        }
+
+        function logout() {
+
+        }
+
+        return {
+            registerUser: registerUser,
+            loginUser: loginUser,
+            logout: logout
+        }
+    }])
